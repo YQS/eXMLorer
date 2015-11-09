fdb4e9a35abb21ba81426231c375d010b8e9a23e
YQS <fsaliceti@yahoo.com.ar>
2015-11-09 01:26:29 -0300

    nuevo __init__, con todos los datos requeridos
    ademas, reordenamiento de funciones, agregada función hasChild y setColumn ahora es más genérica.

--- TagInTree.py
+++ TagInTree.py
@@ -1,33 +1,58 @@
+# encoding: UTF-8
+
+
 class TagInTree(object):
-	def __init__(self, id):
+	def __init__(self, parent_id, id, xmltag, treeview):
 		print 'new TagInTree'
+		#inicializo las variables del objeto
 		self.id = id
 		self.xmltag = None
-		self.treenode = None
-		self.parent_treeview = None
 		self.tagname = ''
 		self.subname = ''
-		
-	def getTag(self):
-		return self.xmltag
-	
+		#self.haschild = None
+		self.treenode = None
+		self.parent_treeview = None
+		##########
+		self.setTag(xmltag)
+		self.setNode(parent_id, id, treeview)
+		self.setColumn('name', self.id)
+		self.setColumn('data', self.getTag().text)
+		self.setColumn('size', self.subname)
+
+	# SETS
 	def setTag(self, xmltag):
 		self.xmltag = xmltag
-		self.tagname = '<' + xmltag.tag + '>'
-		self.subname = getSubnameOfTag(xmltag)
+		self.tagname = '<' + self.xmltag.tag + '>'
+		self.subname = getSubnameOfTag(self.xmltag)
+		#self.haschild = self.hasChild()
+		
+	def setNode(self, parent_id, id, treeview):
+		#Asume que self.xmltag ya está asignado (por ahí conviene hacer una verificación)
+		self.treenode = treeview.insert(parent_id, 'end', id, text= self.tagname + ' ' + getSubnameOfTag(self.xmltag) )
+		self.parent_treeview = treeview
+		
+	def setColumn(self, column, value):
+		self.parent_treeview.set( self.id, column, value )
+	
+	def hasChild(self):
+		xHasChild = False
+		for xChild in self.xmltag:
+			xHasChild = True
+			break
+			
+		return xHasChild
+
+	# GETS
+	def getTag(self):
+		return self.xmltag
 	
 	def getNode(self):
 		return self.treenode
 		
-	def setNode(self, parent_id, id, treeview):
-		self.treenode = treeview.insert(parent_id, 'end', id, text='<' + self.xmltag.tag + '> ' + getSubnameOfTag(self.xmltag) )
-		self.parent_treeview = treeview
-		
 	def getTreeView(self):
 		return self.parent_treeview
 		
-	def setDataColumn(self, value):
-		self.parent_treeview.set( self.id, 'data', value )
+	
 
 
 def getSubnameOfTag(xTag):
@@ -42,13 +67,13 @@ def getSubnameOfTag(xTag):
 		
 	return xName
 
-def generateTagInTree(parent_id, id, xmltag, treeview):
+def generateTagInTree(parent_id, id, xmltag, treeview):			#Deprecated
 	newTagInTree = TagInTree(id)
 	newTagInTree.setTag(xmltag)
 	newTagInTree.setNode(parent_id, id, treeview)
 	
-	treeview.set(id, 'name', id)
-	treeview.set(id, 'data', newTagInTree.getTag().text)
-	treeview.set(id, 'size', newTagInTree.subname)
-
+	newTagInTree.setColumn('name', newTagInTree.id)
+	newTagInTree.setColumn('data', newTagInTree.getTag().text)
+	newTagInTree.setColumn('size', newTagInTree.subname)
+	
 	return newTagInTree
