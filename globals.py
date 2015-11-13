887c8ac7c0c52563f06e87e5fc8125f315e5341d
YQS <fsaliceti@yahoo.com.ar>
2015-11-13 18:16:24 -0300

    Create globals.py

---
+++ globals.py
@@ -0,0 +1,14 @@
+# encoding: UTF-8
+
+# GLOBAL VARIABLES
+
+filename = ''
+XMLTree = None
+XMLParentMap = {}
+dicTagsInTree = {}
+appTreeView = None
+
+buttonWidth = 15
+
+marginToExtendToText = 30
+dataColumnTextLimit = 100
