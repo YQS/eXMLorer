7141a30c2a387eceec747194040584a3e529ffac
YQS <fsaliceti@yahoo.com.ar>
2015-11-18 18:11:48 -0300

    Update globals.py
    - agregado fileNameDic y dicTagSubnames, para manejar los subnames de los tags.
    - añadidos métodos para get y set del diccionario

--- globals.py
+++ globals.py
@@ -12,3 +12,23 @@ buttonWidth = 15
 
 marginToExtendToText = 30
 dataColumnTextLimit = 100
+
+fileNameDic = 'namedic'
+dicTagSubnames = {}
+
+def getDicSubnames():
+	global dicTagSubnames
+	with open(fileNameDic, 'r') as txt:
+		sDic = txt.read()
+		if sDic <> '':
+			dicNames = eval(sDic)
+		else:
+			dicNames = {}
+	#return dicNames
+	dicTagSubnames = dicNames
+	
+def setDicSubnames():
+	global dicTagSubnames
+	with open(fileNameDic, 'w') as txt:
+		txtDic = repr(dicTagSubnames).replace(',' , ',\n\r')
+		txt.write(txtDic)
