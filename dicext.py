8dd44f588d4db7da10ecb4db1f5f8b31f9ad619b
YQS <fsaliceti@yahoo.com.ar>
2015-11-20 18:09:13 -0300

    Create dicext.py
    agregado el modulo dicext.py con la clase que va a usar el global dicTagSubnames, e incluye algunas funciones para levantar de archivo y guardarlo.

---
+++ dicext.py
@@ -0,0 +1,28 @@
+# encoding: UTF-8
+import globals as GL
+
+class DictionaryFile(object):
+	def __init__(self, filename):
+		self.dic = {}
+		self.filename = filename
+		self.getDic()
+		
+	def getDic(self):
+		with open(self.filename, 'r') as txt:
+			sDic = txt.read()
+			if sDic <> '':
+				dicNames = eval(sDic)
+			else:
+				dicNames = {}
+		self.dic = dicNames
+		
+	def save(self):
+		with open(self.filename, 'w') as txt:
+			txtDic = repr(self.dic).replace(',' , ',\n\r')
+			txt.write(txtDic)
+	
+	def updateDic(self, parentTag, childTag):
+		self.dic[parentTag] = childTag
+	
+	def deleteFromDic(self, key):
+		del self.dic[key]
