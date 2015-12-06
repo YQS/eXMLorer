ece240a440a9858ac531e8005085ce10a7d3fb1f
YQS <fsaliceti@yahoo.com.ar>
2015-12-06 03:28:22 -0300

    Update xml_man.py
    - agregado método getEncoding, para obtener el codigo de encodeo del xml y guardarlo en el global XML_encoding. utiliza el modulo re.
    - agregado metodo saveXML, para guardar el xml agregandole la xml declaration y el encodeo con el que estaba guardado previamene el xml.

--- xml_man.py
+++ xml_man.py
@@ -2,6 +2,7 @@
 
 import xml.etree.ElementTree as ET
 import globals as GL
+import re
 
 def getXML(filename):
 	#parseo el xml y instancio el root, para analizarlo todo
@@ -11,10 +12,12 @@ def getXML(filename):
 		return None
 		
 	root = GL.XMLTree.getroot()
+	GL.XML_encoding = getEncoding(filename)
 	GL.XMLParentMap = {c:p for p in GL.XMLTree.iter() for c in p}
 
 	print root.tag
 	print root.attrib
+	#print 'encoding %s' % GL.XML_encoding
 	print
 
 	#funcion para imprimir el tag y todos sus child
@@ -28,6 +31,37 @@ def getXML(filename):
 	showChilds(root, 1)
 	
 	return root
+
+def getEncoding(filename):
+	def regex_search(xPattern, xString):
+		try:
+			xReturn = re.search(xPattern, xString).group(0)
+		except:
+			xReturn = ''
+		finally:
+			return xReturn
+	
+	xEncoding = ''
+	with open(filename, 'r') as xmlfile:
+		first_line = xmlfile.readline()
+		if first_line[0:2] == '<?':
+			xEncoding = regex_search('encoding="(\S)*"', first_line)
+			xEncoding = xEncoding.replace('encoding=', '')
+			xEncoding = xEncoding.replace('"', '')
+			
+			if xEncoding == '':
+				xEncoding = regex_search('encoding=\'(\S)*\'', first_line)
+				xEncoding = xEncoding.replace('encoding=', '')
+				xEncoding = xEncoding.replace('\'', '')
+				
+			
+			
+	return xEncoding			
+
+		
+def saveXML(XMLTree, filename):
+	print 'saving in %s' % filename
+	XMLTree.write(filename, encoding=GL.XML_encoding, xml_declaration=True)
 	
 def newElement(xParent, xTag, xText, xAttrib, xOrder):
 	#xNewElement = ET.SubElement(xParent, xTag, xAttrib)
