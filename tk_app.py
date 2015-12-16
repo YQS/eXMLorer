54323e4d7a4f9fb7bcc8fcd71f297dac0af0da46
YQS <fsaliceti@yahoo.com.ar>
2015-12-16 17:04:22 -0300

    Update tk_app.py
    se modificó el método de apertura de xml para que, en caso de tocar cancelar en el dialogo, no aparezca ningún cartel y se actualice correctamente el label con el filename.

--- tk_app.py
+++ tk_app.py
@@ -116,17 +116,19 @@ def openXML(band_treeview, band_buttons, label_filename):
 	GL.filename = getFilename()
 	label_filename.config(text= GL.filename)
 	
-	root = xml_man.getXML(GL.filename)
-	#root = xml_man.getXML('stylers.xml')
-	
-	if root == None:
-		tkMessageBox.showerror('eXMLorer', 'El archivo %s no es un archivo XML valido' % GL.filename)
-	else:	
-		GL.dicTagsInTree = {}
-		GL.appTreeView = tk_treeview.getTreeView(band_treeview, band_buttons, GL.dicTagsInTree)
+	if GL.filename <> '':
+		root = xml_man.getXML(GL.filename)
+		#root = xml_man.getXML('stylers.xml')
 		
-		GL.dicTagsInTree[root.tag] = TIG.TagInTree('', root.tag, root, None, GL.appTreeView)
-		addXMLToTree(root, root.tag, GL.dicTagsInTree, GL.appTreeView)
+		if root == None:
+			tkMessageBox.showerror('eXMLorer', 'El archivo %s no es un archivo XML valido' % GL.filename)
+			label_filename.config(text= '')
+		else:	
+			GL.dicTagsInTree = {}
+			GL.appTreeView = tk_treeview.getTreeView(band_treeview, band_buttons, GL.dicTagsInTree)
+			
+			GL.dicTagsInTree[root.tag] = TIG.TagInTree('', root.tag, root, None, GL.appTreeView)
+			addXMLToTree(root, root.tag, GL.dicTagsInTree, GL.appTreeView)
 
 def saveXML(mainApp, modo):
 	if modo == 'SAVEAS':
