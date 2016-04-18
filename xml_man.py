c33e7ca28fe80f1ddb8136ec59aaa96084b7c98c
YQS <fsaliceti@yahoo.com.ar>
2016-04-18 15:46:22 -0300

    Update xml_man.py
    - se creó la clase ParserWithComments. Instancia un parser de XML que respeta los comentarios.
    - se modificó la función getXML para que utilice el ParserWithComments
    - se modificó el nombre de la global XML_encoding por XMLEncoding

--- xml_man.py
+++ xml_man.py
@@ -4,20 +4,34 @@ import xml.etree.ElementTree as ET
 import globals as GL
 import re
 
+class ParserWithComments(ET.XMLTreeBuilder):
+	#robado del mismisimo autor del módulo ElementTree
+	#http://effbot.org/zone/element-pi.htm
+	def __init__(self):
+		ET.XMLTreeBuilder.__init__(self)
+		#asumes ElementTree 1.2.X
+		self._parser.CommentHandler = self.handle_comment
+		
+	def handle_comment(self, data):
+		self._target.start(ET.Comment, {})
+		self._target.data(data)
+		self._target.end(ET.Comment)
+
 def getXML(filename):
 	#parseo el xml y instancio el root, para analizarlo todo
 	try:
-		GL.XMLTree = ET.parse(filename)
+		parser = ParserWithComments()
+		GL.XMLTree = ET.parse(filename, parser)
 	except:
 		return None
 		
 	root = GL.XMLTree.getroot()
-	GL.XML_encoding = getEncoding(filename)
+	GL.XMLEncoding = getEncoding(filename)
 	GL.XMLParentMap = {c:p for p in GL.XMLTree.iter() for c in p}
 
 	print root.tag
 	print root.attrib
-	#print 'encoding %s' % GL.XML_encoding
+	#print 'encoding %s' % GL.XMLEncoding
 	print
 
 	#funcion para imprimir el tag y todos sus child
@@ -61,7 +75,7 @@ def getEncoding(filename):
 		
 def saveXML(XMLTree, filename):
 	print 'saving in %s' % filename
-	XMLTree.write(filename, encoding=GL.XML_encoding, xml_declaration=True)
+	XMLTree.write(filename, encoding=GL.XMLEncoding, xml_declaration=True)
 	
 def newElement(xParent, xTag, xText, xAttrib, xOrder):
 	#xNewElement = ET.SubElement(xParent, xTag, xAttrib)
