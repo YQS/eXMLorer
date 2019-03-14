598845f7298deb8e5717db52caab7c66099b769a
YQS <fsaliceti@yahoo.com.ar>
2019-03-13 23:40:27 -0300

    reformateo en idea y se agrega ejecutable para linux (eXMLorer.sh)

--- main.py
+++ main.py
@@ -6,48 +6,50 @@ import globals as GL
 import tk_app
 import module_interface as MOD
 
-def main(mode = 'main', filepath=''):
-	lExcludeMenu = ['button_analyze',
-					'button_glTreeView',
-					'button_dicSubnames',
-					'button_getDicSubnames',
-					'button_printEncoding',
-					'button_printPrettyPrint',
-					'button_showCurrentSearch',
-					'button_showDicTagsInTree',
-					'button_showXMLParentMap',
-					'button_showCaseSensitive',
-					'button_captureStringXML',
-					'button_showSearchResult', 
-					'button_showSearchStartingPoint', 
-					'button_lastFolder',
-					'button_foldTest',
-					'button_showChildQty',
-					]
 
-	GL.globalVarsStart()
-	MOD.startModules()
-	
-	mainApp = tk_app.MainApp(lExcludeMenu=lExcludeMenu)
-	#if filepath == None:
-	#	tk_app.openXML(mainApp)
-	#else:
-	if filepath <> None:
-		tk_app.openXML(mainApp, filename=filepath)
-	
-	mainApp.focus_set()
-	mainApp.mainloop()
-	
-	try:
-		mainApp.destroy()
-	except:
-		pass
+def main(mode='main', filepath=''):
+    lExcludeMenu = ['button_analyze',
+                    'button_glTreeView',
+                    'button_dicSubnames',
+                    'button_getDicSubnames',
+                    'button_printEncoding',
+                    'button_printPrettyPrint',
+                    'button_showCurrentSearch',
+                    'button_showDicTagsInTree',
+                    'button_showXMLParentMap',
+                    'button_showCaseSensitive',
+                    'button_captureStringXML',
+                    'button_showSearchResult',
+                    'button_showSearchStartingPoint',
+                    'button_lastFolder',
+                    'button_foldTest',
+                    'button_showChildQty',
+                    ]
+
+    GL.globalVarsStart()
+    MOD.startModules()
+
+    mainApp = tk_app.MainApp(lExcludeMenu=lExcludeMenu)
+    # if filepath == None:
+    #	tk_app.openXML(mainApp)
+    # else:
+    if filepath <> None:
+        tk_app.openXML(mainApp, filename=filepath)
+
+    mainApp.focus_set()
+    mainApp.mainloop()
+
+    try:
+        mainApp.destroy()
+    except:
+        pass
+
 
 if __name__ == '__main__':
-	#parsing CLI arguments
-	parser = argparse.ArgumentParser()
-	parser.add_argument('filepath', nargs="?")
-	
-	args = parser.parse_args()
-	
-	main(filepath=args.filepath)
+    # parsing CLI arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filepath', nargs="?")
+
+    args = parser.parse_args()
+
+    main(filepath=args.filepath)
